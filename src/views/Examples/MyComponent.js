7ab2f5da433e3478caef35264f41eaa6454f96ff
blank201611 <gaoblack9898@gmail.com>
2024-03-15 10:21:08 +0700

    update: Form, DOM EVENT

--- src/views/Examples/MyComponent.js
+++ src/views/Examples/MyComponent.js
@@ -2,24 +2,45 @@ import React from "react";
 
 class MyComponent extends React.Component {
   state = {
-    name: "DOAN TRUNG HIEU",
+    firstName: "",
+    lastName: "",
   };
 
-  handleOnChangeInput = (event) => {
+  handleChangeFirstName = (e) => {
     this.setState({
-      name: event.target.value,
+      firstName: e.target.value,
     });
+  };
+  handleChangeLastName = (e) => {
+    this.setState({
+      lastName: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert("click me");
   };
 
   render() {
+    console.log(">>> Call Render: ", this.state);
     return (
       <React.Fragment>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={(event) => this.handleOnChangeInput(event)}
-        />
-        <h1>My Name is {this.state.name}</h1>
+        <form action="">
+          <label htmlFor="fname">First Name:</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.firstName}
+            onChange={(e) => this.handleChangeFirstName(e)}
+          />
+          <br />
+          <label htmlFor="lname">Last Name:</label>
+          <br />
+          <input type="text" onChange={(e) => this.handleChangeLastName(e)} />
+          <br />
+          <button onClick={(e) => this.handleSubmit(e)}>Submit</button>
+        </form>
+        <h1>Hello World</h1>
       </React.Fragment>
     );
   }
