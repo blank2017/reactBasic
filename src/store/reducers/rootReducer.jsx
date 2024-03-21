6e6ad387dc530eb35b3fd46780516a3f3461c853
blank201611 <gaoblack9898@gmail.com>
2024-03-21 15:10:23 +0700

    Update Redux lib to React

---
+++ src/store/reducers/rootReducer.jsx
@@ -0,0 +1,11 @@
+const initState = {
+  users: [
+    { id: 1, name: "John Doe", email: "john@doe.com" },
+    { id: 2, name: "Jane Smith", email: "jane@smith.com" },
+  ],
+};
+const rootReducer = (state = initState, action) => {
+  return;
+};
+
+export default rootReducer;
