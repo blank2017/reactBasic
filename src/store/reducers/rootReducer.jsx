0718544a547903d6d36b6d0b21b765a326af92b1
blank201611 <gaoblack9898@gmail.com>
2024-03-21 16:21:08 +0700

    Update Action of Redux with 2 funtion mapSatateToProps() and mapDispatchToProps() and import {connect} from react-redux lib

--- src/store/reducers/rootReducer.jsx
+++ src/store/reducers/rootReducer.jsx
@@ -3,9 +3,37 @@ const initState = {
     { id: 1, name: "John Doe", email: "john@doe.com" },
     { id: 2, name: "Jane Smith", email: "jane@smith.com" },
   ],
+  post: [],
 };
 const rootReducer = (state = initState, action) => {
-  return;
+  switch (action.type) {
+    case "DELETE_USER":
+      console.log(">>> Run Into Init User: ", action.payload);
+      const newUsersList = state.users.filter(
+        (user) => user.id !== action.payload.id
+      );
+      const currentUser = state.users.filter(
+        (user) => user.id === action.payload.id
+      );
+      return {
+        ...state,
+        post: [...state.post, currentUser], // keep the original
+        users: newUsersList,
+      };
+    case "CREATE_USER":
+      const id = Math.floor(Math.random() * 1000);
+      let user = {
+        id: id,
+        name: `random - ${id}`,
+        email: `random${id}@gmail.com`,
+      };
+      return {
+        ...state,
+        users: [...state.users, user],
+      };
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
--- src/views/Examples/Home.js
+++ src/views/Examples/Home.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+class Home extends React.Component {
+  handleDeleteUser = (user) => {
+    console.log(">>>Check ing user to delete", user);
+    this.props.deleteDataInStore(user);
+  };
+  handleAddNewUser = () => {
+    this.props.addNewUserToStore();
+  };
+  render() {
+    const users = this.props.dataRedux;
+    return (
+      <React.Fragment>
+        <h1>Home Page</h1>
+        <button onClick={() => this.handleAddNewUser()}>ADD NEW USER</button>
+        {users.map((user, index) => {
+          return (
+            <div
+              className="user-item"
+              key={user.id}
+              style={{ display: "flex", gap: "10px" }}
+            >
+              <span>
+                {user.name} - {user.email}
+              </span>
 
-class Home  extends React.Component {
-
-    render() {
-        return(
-            <h1>Home Page</h1>
-        )
-    }
+              <button onClick={() => this.handleDeleteUser(user)}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </React.Fragment>
+    );
+  }
 }
 
-export  default Home;
+const mapStateToProps = (state) => {
+  return {
+    dataRedux: state.users, //Data in Redux store
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteDataInStore: (userDelete) =>
+      dispatch({ type: "DELETE_USER", payload: userDelete }),
+    addNewUserToStore: () => dispatch({ type: "CREATE_USER" }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
